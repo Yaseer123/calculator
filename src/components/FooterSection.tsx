787e135b250ced4f47d5f939b405aaac7d163d04
Yaseer123 <yaseerarafat230@gmail.com>
2024-11-02 16:09:25 +0600

    final

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const FooterSection = () => {
     return (
-        <footer className="mt-12 border-t p-6 text-center text-gray-600">
+        <footer className="mt-12 border-t p-6 text-center text-white">
             <ul className="flex flex-wrap justify-center space-x-4">
                 <li>
-                    <Link href="/home">Home</Link>
+                    <Link href="/">Home</Link>
                 </li>
                 <li>
                     <Link href="/about-us">About Us</Link>
@@ -15,10 +15,10 @@ const FooterSection = () => {
                     <Link href="/privacy-policy">Privacy Policy</Link>
                 </li>
                 <li>
-                    <Link href="/loan-tables">Loan Tables</Link>
+                    <Link href="/inflation-calculator">Inflation</Link>
                 </li>
                 <li>
-                    <Link href="/car-loan">Car Loan</Link>
+                    <Link href="/buy_home">How much can i afford for home</Link>
                 </li>
             </ul>
         </footer>
